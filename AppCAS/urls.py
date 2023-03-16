cf10d974fdee3924fd1927fc1b08ea609bca8719
juanmina757 <juan.mina757@misena.edu.co>
2023-03-16 09:53:54 -0500

    agregar views login

--- AppCAS/urls.py
+++ AppCAS/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns=[
-    path('', views.home ,name="home.html"),
+    path('', views.home ,name="main"),
+    path('ingresar/', views.login ,name="login"),
        
 ]
